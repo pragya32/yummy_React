7e5e007dca79b32e64f77f0e9b88dae84a7645f8
Pragya Shukla <shuklapragya495@gmail.com>
2021-02-07 00:38:02 +0530

    6  one

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ const App = () => {
   const getSearch = (e) => {
     e.preventDefault();
     setQuery(search);
+    setSearch("");
   };
   return (
     <div className="App">
@@ -48,6 +49,7 @@ const App = () => {
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
         />
       ))}
     </div>
--- src/Recipie.js
+++ src/Recipie.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const Recipie = ({ title, calories, image }) => {
+const Recipie = ({ title, calories, image, ingredients }) => {
   return (
     <div>
       <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredients) => {
+          <li>
+            <p>{ingredients.text}</p>
+          </li>;
+        })}
+      </ol>
       <p>{calories}</p>
       <img src={image} alt="" />
     </div>
